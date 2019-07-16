7632cf19832c4b7aacfe9442494d523c9611dbb2
Brian Kessler <kessler.bm@gmail.com>
2019-07-16 08:21:04 +0200

    Some refactoring for clarity and to fix corner case.

--- apps/auction/lib/auction/sfdc_repo.ex
+++ apps/auction/lib/auction/sfdc_repo.ex
@@ -22,26 +22,34 @@ defmodule Auction.SfdcRepo do
     to_item(sfdc_item)
   end
 
-  def get_by(Item, target_attributes) do
+  def get_by(Item, %{} = target_attributes) do
     where_clause = build_where_clause(target_attributes)
     query = @basic_query <> where_clause
     {:ok, %ExForce.QueryResult{records: records}} = ExForce.query(@sfdc_client, query)
     to_items(records)
   end
 
-  defp build_where_clause(target_attributes) do
+  defp build_where_clause(%{} = target_attributes) do
     target_keys = Map.keys(target_attributes)
-    where_phrase_list = []
+    where_phrase_list = build_where_phrase_list(target_attributes, Map.keys(target_attributes))
+    build_where_clause(where_phrase_list)
+  end
 
-    where_phrase_list =
-      for (target_key <- target_keys) do
-        [build_where_phrase(target_attributes, target_key) | where_phrase_list]
-      end
+  defp build_where_clause(where_phrase_list = []) do
+    ""
+  end
 
+  defp build_where_clause(where_phrase_list) when is_list(where_phrase_list) do
     " WHERE " <> Enum.join(where_phrase_list, " AND ")
   end
 
-  def build_where_phrase(target_attributes, target_key) do
+  def build_where_phrase_list(%{} = target_attributes, target_keys, where_phrase_list \\ []) do
+    for (target_key <- target_keys) do
+      [build_where_phrase(target_attributes, target_key) | where_phrase_list]
+    end
+  end
+
+  def build_where_phrase(%{} = target_attributes, target_key) do
     sfdc_key = @sfdc_key_map[target_key]
     target_value = target_attributes[target_key]
     "#{sfdc_key} = '#{target_value}'"

1e86094f6e6376e3ac8514e3e1e1b2ceebd9c3b9
Brian Kessler <kessler.bm@gmail.com>
2019-07-16 08:47:22 +0200

    Went back to Postgres database. Added insert and delete methods.

--- apps/auction/lib/auction.ex
+++ apps/auction/lib/auction.ex
@@ -1,9 +1,18 @@
 defmodule Auction do
   alias Auction.Item
 
-  @repo Auction.SfdcRepo
+  #  @repo Auction.SfdcRepo
+  @repo Auction.Repo
 
   def list_items, do: @repo.all(Item)
   def get_item(id), do: @repo.get!(Item, id)
   def get_items_by(attributes), do: @repo.get_by(Item, attributes)
+
+  def insert_item(attributes) do
+    Auction.Item
+    |> struct(attributes)
+    |> @repo.insert()
+  end
+
+  def delete_item(%Auction.Item{} = item), do: @repo.delete(item)
 end

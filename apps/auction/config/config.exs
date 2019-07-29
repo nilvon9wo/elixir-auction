2fd948f56f536c406ffd2c5828439f12eef39e83
nilvon9wo <kessler.bm@gmail.com>
2019-07-29 13:38:19 +0200

    Added missing ecto repo configuration stuff.

--- apps/auction/config/config.exs
+++ apps/auction/config/config.exs
@@ -1,2 +1,8 @@
 use Mix.Config
-import_config "#{Mix.env()}.exs"
+
+import_config "../apps/*/config/config.exs"
+import_config "#{Mix.env}.exs"
+
+config :phoenix, :json_library, Jason
+config :auction, ecto_repos: [Auction.Repo]
+
